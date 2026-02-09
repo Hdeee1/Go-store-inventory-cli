9a1dcda68b57623564154e43eeb87d9631ff5aaf
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2026-02-09 14:37:23 +0700

    Refactor main function to include user input for item display and search options; enhance ShowAllItems and SearchItem functions for better user interaction

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "github.com/Hdeee1/go-store-inventory-CLI/services"
+import (
+	"fmt"
+	"os"
+
+	"github.com/Hdeee1/go-store-inventory-CLI/services"
+)
 
 func main() {
-	services.ShowAllItems()
-	services.SearchItem()
+	services.InitData()
+	for {
+		var choice int
+		fmt.Println("1. Show All Items")
+		fmt.Println("2. Search Item")
+		fmt.Println("3. Quit")
+
+		fmt.Println("Chose options:")
+		fmt.Scanln(&choice)
+
+		switch choice {
+		case 1 :
+			services.ShowAllItems()
+			return
+		case 2:
+			services.SearchItem()
+			return
+		case 3:
+			os.Exit(0)
+		default:
+			fmt.Println("")
+		}
+	}
 }
--- services/service.go
+++ services/service.go
@@ -8,23 +8,26 @@ import (
 
 var Products []models.Product
 
-func ShowAllItems() {
+func InitData() {
 	products := []models.Product{
 		{Name: "Laptop", Price: 50000000, Stock: 10},
 		{Name: "Monitor", Price: 10000000, Stock: 3},
 		{Name: "Mouse", Price: 100000, Stock: 0},
 	}
 	Products = append(Products, products...)
+}
+
+func ShowAllItems() {
 	
-	// for _, item := range Products {
-	// 	if item.Stock == 0 {
-	// 		fmt.Printf("Product Name: %v, Price: %v, Stock: Sold Out \n", item.Name, item.Price)
-	// 	} else if item.Stock > 5 {
-	// 		fmt.Printf("Product Name: %v, Price: %v, Stock: Low Stock\n", item.Name, item.Price)
-	// 	} else {
-	// 		fmt.Printf("Product Name: %v, Price: %v, Stock: Available\n", item.Name, item.Price)
-	// 	}
-	// }
+	for _, item := range Products {
+		if item.Stock == 0 {
+			fmt.Printf("Product Name: %v, Price: %v, Stock: Sold Out \n", item.Name, item.Price)
+		} else if item.Stock > 5 {
+			fmt.Printf("Product Name: %v, Price: %v, Stock: Low Stock\n", item.Name, item.Price)
+		} else {
+			fmt.Printf("Product Name: %v, Price: %v, Stock: Available\n", item.Name, item.Price)
+		}
+	}
 }
 
 func SearchItem() {
@@ -34,7 +37,7 @@ func SearchItem() {
 	}
 
 	var userInput string
-	fmt.Printf("Search item name: ")
+	fmt.Printf("Search item name (ex: Laptop): ")
 	fmt.Scanln(&userInput)
 
 	searchName := userInput
